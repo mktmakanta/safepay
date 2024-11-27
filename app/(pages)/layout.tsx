d7ee15c7ce58c40cb3980ef0cb5555848221ce34
mktmakanta <mktmakantamkt@gmail.com>
2024-11-27 01:48:18 +0100

    working onn the sidebar

--- app/(pages)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ToastProvider } from '@radix-ui/react-toast'
-
-export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <ToastProvider>{children}</ToastProvider>
-    </>
-  )
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 'use client'
+import Provider from './Redux/AuthProvider/sessionProvider'
+
+import './globals.css'
+// import { Jost } from 'next/font/google'
+import { ToastProvider } from '@radix-ui/react-toast'
+
+// const jost = Jost({
+//   subsets: ['latin'],
+//   weight: ['400', '700'],
+//   variable: '--font-jost',
+// })
 
 interface Metadata {
   title: string
   description: string
 }
-// import type { Metadata } from 'next'
-import './styles/globals.css'
-import { Jost } from 'next/font/google'
-import { ToastProvider } from '@radix-ui/react-toast'
-import Provider from './api/sessionProvider'
-
-const jost = Jost({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-jost',
-})
 
 export const Metadata = {
   title: 'safepay',
@@ -32,7 +32,7 @@ export default function RootLayout({
         <link rel="icon" href="../assets/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
-      <body className={jost.variable}>
+      <body>
         <Provider>
           <ToastProvider>
             <main className="main">{children}</main>
