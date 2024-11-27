0e78201c044f0b5c240b2ca75eac85e9fcefde84
mktmakanta <mktmakantamkt@gmail.com>
2024-11-27 22:47:37 +0100

    working on the profile section now

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,5 +67,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar'),
+    require('tailwind-scrollbar-hide'),
+  ],
 }
